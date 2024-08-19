8e65be1d89820e865b23badb1ee03aa0dc73162a
chuxin <15115359055@139.com>
2024-08-19 10:42:05 +0800

    feat: 路由快速上手 和 路由懒加载

--- demo-react-router-dom-v6/src/main.tsx
+++ demo-react-router-dom-v6/src/main.tsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Suspense, LazyExoticComponent, lazy } from "react";
+import {
+  createHashRouter, //  hash 模式
+  // createBrowserRouter,// history 模式
+  RouterProvider, //
+  Link, // 路由跳转
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+interface ILazyImportComponent {
+  lazyChildren: LazyExoticComponent<() => JSX.Element>;
+}
+const LazyImportComponent = (props: ILazyImportComponent) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <props.lazyChildren />
+    </Suspense>
+  );
+};
+
+function Router() {
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          home <Link to="about">go to about</Link>
+        </>
+      ),
+    },
+    {
+      path: "/about",
+      element: (
+        <>
+          about<Link to="/">go to home</Link>
+        </>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <LazyImportComponent
+          lazyChildren={lazy(() => import("@/pages/Login/index.tsx"))}
+        />
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <Router />
+  </>
+);
